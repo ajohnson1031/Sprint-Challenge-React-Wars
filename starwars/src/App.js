2e3817e1fcdf89f6043786ce9e72977807e83210
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-08-05 06:57:11 -0400

    optimized code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,38 +5,12 @@ import StarCard from "./components/StarCard";
 import "./App.css";
 
 const App = () => {
-  const [card1, setCard1] = useState();
-  const [card2, setCard2] = useState();
-  const [card3, setCard3] = useState();
-  const [card4, setCard4] = useState();
-  const [card5, setCard5] = useState();
-  const [card6, setCard6] = useState();
-  const [card7, setCard7] = useState();
-  const [card8, setCard8] = useState();
-  const [card9, setCard9] = useState();
-  const [card10, setCard10] = useState();
-
-  const stateHolder = [
-    setCard1,
-    setCard2,
-    setCard3,
-    setCard4,
-    setCard5,
-    setCard6,
-    setCard7,
-    setCard8,
-    setCard9,
-    setCard10
-  ];
+  const [cardData, setCardData] = useState();
 
   useEffect(() => {
     axios
       .get("https://swapi.co/api/people/")
-      .then(res =>
-        res.data.results.forEach((r, i) => {
-          stateHolder[i](r);
-        })
-      )
+      .then(res => setCardData(res.data.results))
       .catch(err => console.log(`Error: `, err));
   }, []);
 
@@ -44,16 +18,10 @@ const App = () => {
     <div className="App">
       <h1 className="Header">React Wars</h1>
       <Container className="main-container">
-        <StarCard data={card1} />
-        <StarCard data={card2} />
-        <StarCard data={card3} />
-        <StarCard data={card4} />
-        <StarCard data={card5} />
-        <StarCard data={card6} />
-        <StarCard data={card7} />
-        <StarCard data={card8} />
-        <StarCard data={card9} />
-        <StarCard data={card10} />
+        {cardData &&
+          cardData.map(res => {
+            return <StarCard data={res} key={res.created} />;
+          })}
       </Container>
     </div>
   );
