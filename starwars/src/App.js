522880b46f6c20a66272ee17b00ffbece48f365e
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-08-05 23:47:33 -0400

    updated app with next and previous buttons to cycle through pages

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,22 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Container, Select } from "semantic-ui-react";
+import swapiModule from "./SWAPI-Wrapper/lib/swapi";
+import { Container } from "semantic-ui-react";
 import StarCard from "./components/StarCard";
 import "./App.css";
 
 const App = () => {
   const [cardData, setCardData] = useState();
+  const [pageState, setPageState] = useState(1);
+  const [prevState, setPrevState] = useState("hidden");
+  const [nextState, setNextState] = useState("hidden");
 
   useEffect(() => {
-    axios
-      .get("https://swapi.co/api/people/")
-      .then(res => setCardData(res.data.results))
-      .catch(err => console.log(`Error: `, err));
-  }, []);
+    swapiModule.getPeople({ page: pageState }, res => setCardData(res.results));
+  }, [pageState]);
+
+  useEffect(() => {
+    pageState <= 1 ? setPrevState("hidden") : setPrevState("visible");
+    pageState >= 9 ? setNextState("hidden") : setNextState("visible");
+  }, [pageState]);
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Container className="header-container">
+        <h1 className="Header">React Wars</h1>
+        <Container className="arrow-container">
+          <p
+            className="prev"
+            onClick={() => {
+              setPageState(pageState - 1);
+            }}
+            style={{ visibility: prevState }}
+          >
+            prev
+          </p>
+          <p
+            className="next"
+            onClick={() => {
+              setPageState(pageState + 1);
+            }}
+            style={{ visibility: nextState }}
+          >
+            next
+          </p>
+        </Container>
+      </Container>
       <Container className="main-container">
         {cardData &&
           cardData.map(res => {
---
+++ starwars/src/App_bak.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Select } from "semantic-ui-react";
+import StarCard from "./components/StarCard";
+import "./App.css";
+
+const App = () => {
+  const [cardData, setCardData] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => setCardData(res.data.results))
+      .catch(err => console.log(`Error: `, err));
+  }, []);
+
+  cardData && console.log(cardData);
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+      <Container className="main-container">
+        {cardData &&
+          cardData.map(res => {
+            return <StarCard data={res} key={res.created} />;
+          })}
+      </Container>
+    </div>
+  );
+};
+
+export default App;
